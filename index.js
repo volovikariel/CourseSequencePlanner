d2ebb831fe289cc6c3cc28e96414535b0c1a8187
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-11-11 12:00:36 -0500

    minor code formatting change

--- index.js
+++ index.js
@@ -221,17 +221,17 @@ function addEdge(fromX, fromY, toX, toY, isPrereq) {
   document.getElementById('root-svg').innerHTML += `
         <svg>
             <defs>
-            < <marker
-                    id="head"
-                    viewBox="0 0 10 10"
-                    refX="5"
-                    refY="5"
-                    markerWidth="6"
-                    markerHeight="6"
-                    orient="auto-start-reverse">
-                    <path d="M 0 0 L 10 5 L 0 10 z" />
-                </marker>
-      
+              <marker
+                  id="head"
+                  viewBox="0 0 10 10"
+                  refX="5"
+                  refY="5"
+                  markerWidth="6"
+                  markerHeight="6"
+                  orient="auto-start-reverse"
+              >
+                <path d="M 0 0 L 10 5 L 0 10 z" />
+              </marker>
             </defs>
 
             <path
