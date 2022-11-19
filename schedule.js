05d51e1ccedb7b8a1a5cc5d8835d08fc281611fa
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-11-19 18:19:55 -0500

    format code with eslint

--- schedule.js
+++ schedule.js
@@ -1,9 +1,9 @@
 const courseScheduleInfo = {
   COMP248: {
-    courseName: "Object-Oriented Programming 1",
-    courseCode: "COMP248",
-    startTime: "10:00",
-    endTime: "18:30",
+    courseName: 'Object-Oriented Programming 1',
+    courseCode: 'COMP248',
+    startTime: '10:00',
+    endTime: '18:30',
     mon: true,
     tue: false,
     wed: true,
@@ -11,10 +11,10 @@ const courseScheduleInfo = {
     fri: false,
   },
   COMP249: {
-    courseName: "Object-Oriented Programming 2",
-    courseCode: "COMP249",
-    startTime: "13:30",
-    endTime: "15:30",
+    courseName: 'Object-Oriented Programming 2',
+    courseCode: 'COMP249',
+    startTime: '13:30',
+    endTime: '15:30',
     mon: true,
     tue: false,
     wed: true,
@@ -23,8 +23,8 @@ const courseScheduleInfo = {
   },
 };
 
-// How do I use this to modify the css 
-const colors = ["#ff0000", "#d1a94c", "#118ab2", "#06d6a0"];
+// How do I use this to modify the css
+const colors = ['#ff0000', '#d1a94c', '#118ab2', '#06d6a0'];
 
 function getCourseSchedule(courseCode) {
   return courseScheduleInfo[courseCode];
@@ -32,9 +32,9 @@ function getCourseSchedule(courseCode) {
 
 function translateDay(courseCode) {
   // format weekday bools to numbers for timeslots
-  let course = getCourseSchedule(courseCode);
-  let weekdays = [course.mon, course.tue, course.wed, course.thu, course.fri];
-  let weekdaysNum = [];
+  const course = getCourseSchedule(courseCode);
+  const weekdays = [course.mon, course.tue, course.wed, course.thu, course.fri];
+  const weekdaysNum = [];
   for (let i = 0; i < weekdays.length; i++) {
     if (weekdays[i] == true) {
       weekdaysNum.push(i);
@@ -46,9 +46,9 @@ function translateDay(courseCode) {
 function translateTime(hhmm) {
   // Format time to numbers for 'slices' of timeslots, goes from 9am to 630pm (9.5 hours) in 30min intervals,
   // Meaning that 9 is the first timeslot = 0 , 9:30 => 1, 9:45 => 1.5, 10 => 2, etc.
-  let hours = parseInt(hhmm.split(":")[0]);
-  let minutes = parseInt(hhmm.split(":")[1]) / 60;
-  let time = hours + minutes;
+  const hours = parseInt(hhmm.split(':')[0]);
+  const minutes = parseInt(hhmm.split(':')[1]) / 60;
+  const time = hours + minutes;
   let x;
 
   if (time < 18) {
@@ -58,25 +58,25 @@ function translateTime(hhmm) {
 }
 
 function translateTimeForSlots(courseCode) {
-  let course = getCourseSchedule(courseCode);
-  let timeSlotStart = translateTime(course.startTime);
-  let timeSlotEnd = translateTime(course.endTime);
+  const course = getCourseSchedule(courseCode);
+  const timeSlotStart = translateTime(course.startTime);
+  const timeSlotEnd = translateTime(course.endTime);
   return [timeSlotStart, timeSlotEnd];
 }
 
 function addSelectedCourseToSchedule(courseCode) {
   // Add the selected course to the schedule
-  let course = getCourseSchedule(courseCode);
-  let days = translateDay(courseCode);
-  let times = translateTimeForSlots(courseCode);
-  let startTime = times[0];
-  let endTime = times[1];
+  const course = getCourseSchedule(courseCode);
+  const days = translateDay(courseCode);
+  const times = translateTimeForSlots(courseCode);
+  const startTime = times[0];
+  const endTime = times[1];
 
   for (let i = 0; i < days.length; i++) {
-    let day = days[i];
+    const day = days[i];
 
     // Add the course to the schedule
-    document.getElementById("courses").innerHTML += ` 
+    document.getElementById('courses').innerHTML += ` 
         style="
         --day: "${day}"; 
         --timeslot-start: "${startTime}";
@@ -91,6 +91,5 @@ function addSelectedCourseToSchedule(courseCode) {
 }
 
 // Lets say they selected 248 and 249
-addSelectedCourseToSchedule("COMP248");
-addSelectedCourseToSchedule("COMP249");
-
+addSelectedCourseToSchedule('COMP248');
+addSelectedCourseToSchedule('COMP249');
