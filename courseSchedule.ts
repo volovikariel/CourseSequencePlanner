8c166a961e304e17e244560fe12f917b04cbdd90
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-12-03 17:06:10 -0500

    add seeding code for the course schedules, as well as types for the course schedules in both Prisma and Typescript (using Zod)

---
+++ courseSchedule.ts
@@ -0,0 +1,67 @@
+import { z } from "zod";
+
+export type CourseScheduleAPIResponse = {
+    [key in keyof CourseSchedule]: string
+}
+
+export async function fetchCourseSchedules(): Promise<CourseScheduleAPIResponse[]> {
+    // Basic access authentication credentials https://en.wikipedia.org/wiki/Basic_access_authentication
+    const opendataCredentials = Buffer.from(`${process.env.OPENDATA_USER}:${process.env.OPENDATA_PSWRD}`).toString('base64');
+    const headers = {
+        headers: {
+            'Authorization': `Basic ${opendataCredentials}`,
+        }
+    };
+    const courseScheduleURL = 'https://opendata.concordia.ca/API/v1/course/schedule/filter/*/*/*';
+
+    return await fetch(courseScheduleURL, headers).then(res => res.json());
+}
+
+function validateIsNumberAndReturnValue() {
+    return z.string().regex(/^\d*$/, 'Must be a number').transform(Number);
+}
+export const CourseScheduleSchema = z.object({
+    courseID: validateIsNumberAndReturnValue(),
+    termCode: validateIsNumberAndReturnValue(),
+    session: z.string(),
+    subject: z.string(),
+    catalog: z.string(),
+    section: z.string(),
+    componentCode: z.string(),
+    componentDescription: z.string(),
+    classNumber: validateIsNumberAndReturnValue(),
+    classAssociation: validateIsNumberAndReturnValue(),
+    courseTitle: z.string(),
+    topicID: validateIsNumberAndReturnValue(),
+    topicDescription: z.string(),
+    classStatus: z.string(),
+    locationCode: z.string(),
+    instructionModeCode: z.string(),
+    instructionModeDescription: z.string(),
+    meetingPatternNumber: validateIsNumberAndReturnValue(),
+    roomCode: z.string(),
+    buildingCode: z.string(),
+    room: z.string(),
+    classStartTime: z.string(),
+    classEndTime: z.string(),
+    modays: z.string(),
+    tuesdays: z.string(),
+    wednesdays: z.string(),
+    thursdays: z.string(),
+    fridays: z.string(),
+    saturdays: z.string(),
+    sundays: z.string(),
+    classStartDate: z.string(),
+    classEndDate: z.string(),
+    career: z.string(),
+    departmentCode: z.string(),
+    departmentDescription: z.string(),
+    facultyCode: z.string(),
+    facultyDescription: z.string(),
+    enrollmentCapacity: validateIsNumberAndReturnValue(),
+    currentEnrollment: z.string(),
+    waitlistCapacity: validateIsNumberAndReturnValue(),
+    currentWaitlistTotal: z.string(),
+    hasSeatReserved: z.string()
+}).strict();
+export type CourseSchedule = z.infer<typeof CourseScheduleSchema>
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,49 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
+import { PrismaClient } from '@prisma/client';
+import { CourseScheduleAPIResponse, CourseScheduleSchema, fetchCourseSchedules } from '../courseSchedule.js';
+const prisma = new PrismaClient();
 
+async function main() {
+  // Delete all entries
+  await prisma.courseSchedule.deleteMany();
+
+  // Insert all course schedule values
+  const rawCourseScheduleData = await fetchCourseSchedules();
+  const rawCourseScheduleDataFrom2022 = rawCourseScheduleData.filter((course: CourseScheduleAPIResponse) => course.classStartDate.endsWith('2022'));
+  const courseSchedules = [];
+  for (const rawCourseSchedule of rawCourseScheduleDataFrom2022) {
+    const result = CourseScheduleSchema.safeParse(rawCourseSchedule);
+    if (result.success) {
+      console.log(result.data);
+      courseSchedules.push(result.data);
+    }
+    else {
+      console.log(result.error);
+      console.log(rawCourseSchedule);
+      return;
+    }
+  }
+  console.log(`#Rows to add: ${courseSchedules.length}`);
+  const creates = [];
+  const rowIds = new Set();
+  for (const [rowNum, courseSchedule] of courseSchedules.entries()) {
+    const rowId = `(${courseSchedule.subject}, ${courseSchedule.catalog}, ${courseSchedule.termCode}, ${courseSchedule.section}, ${courseSchedule.meetingPatternNumber})`;
+    console.log(`Inserting row #${rowNum}: ${rowId}`);
+    if (rowIds.has(rowId)) {
+      console.log(`Found duplicate row ${rowId}`);
+      return;
+    } else {
+      rowIds.add(rowId);
+    }
+    creates.push(prisma.courseSchedule.create({ data: courseSchedule }));
+  }
+  prisma.$transaction(creates);
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
